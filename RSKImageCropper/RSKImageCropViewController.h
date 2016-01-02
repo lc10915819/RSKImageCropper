78dcba6b7a4776d250c34db93f4d4aa4cb6144d1
chanli <10915819@qq.com>
2016-01-02 14:29:05 +0800

    fix 取消 确定的位置。

--- RSKImageCropper/RSKImageCropViewController.h
+++ RSKImageCropper/RSKImageCropViewController.h
@@ -33,27 +33,30 @@ NS_ASSUME_NONNULL_BEGIN
  Types of supported crop modes.
  */
 typedef NS_ENUM(NSUInteger, RSKImageCropMode) {
-    RSKImageCropModeCircle,
-    RSKImageCropModeSquare,
-    RSKImageCropModeCustom
+  RSKImageCropModeCircle,
+  RSKImageCropModeSquare,
+  RSKImageCropModeCustom
 };
 
 @interface RSKImageCropViewController : UIViewController
 
 /**
- Designated initializer. Initializes and returns a newly allocated view controller object with the specified image.
- 
+ Designated initializer. Initializes and returns a newly allocated view
+ controller object with the specified image.
+
  @param originalImage The image for cropping.
  */
 - (instancetype)initWithImage:(UIImage *)originalImage;
 
 /**
- Initializes and returns a newly allocated view controller object with the specified image and the specified crop mode.
- 
+ Initializes and returns a newly allocated view controller object with the
+ specified image and the specified crop mode.
+
  @param originalImage The image for cropping.
  @param cropMode The mode for cropping.
  */
-- (instancetype)initWithImage:(UIImage *)originalImage cropMode:(RSKImageCropMode)cropMode;
+- (instancetype)initWithImage:(UIImage *)originalImage
+                     cropMode:(RSKImageCropMode)cropMode;
 
 ///-----------------------------
 /// @name Accessing the Delegate
@@ -61,17 +64,22 @@ typedef NS_ENUM(NSUInteger, RSKImageCropMode) {
 
 /**
  The receiver's delegate.
- 
- @discussion A `RSKImageCropViewControllerDelegate` delegate responds to messages sent by completing / canceling crop the image in the image crop view controller.
+
+ @discussion A `RSKImageCropViewControllerDelegate` delegate responds to
+ messages sent by completing / canceling crop the image in the image crop view
+ controller.
  */
-@property (weak, nonatomic, nullable) id<RSKImageCropViewControllerDelegate> delegate;
+@property(weak, nonatomic, nullable) id<RSKImageCropViewControllerDelegate>
+    delegate;
 
 /**
  The receiver's data source.
- 
- @discussion A `RSKImageCropViewControllerDataSource` data source provides a custom rect and a custom path for the mask.
+
+ @discussion A `RSKImageCropViewControllerDataSource` data source provides a
+ custom rect and a custom path for the mask.
  */
-@property (weak, nonatomic, nullable) id<RSKImageCropViewControllerDataSource> dataSource;
+@property(weak, nonatomic, nullable) id<RSKImageCropViewControllerDataSource>
+    dataSource;
 
 ///--------------------------
 /// @name Accessing the Image
@@ -80,40 +88,43 @@ typedef NS_ENUM(NSUInteger, RSKImageCropMode) {
 /**
  The image for cropping.
  */
-@property (strong, nonatomic) UIImage *originalImage;
+@property(strong, nonatomic) UIImage *originalImage;
 
 /// -----------------------------------
 /// @name Accessing the Mask Attributes
 /// -----------------------------------
 
 /**
- The color of the layer with the mask. Default value is [UIColor colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.7f].
+ The color of the layer with the mask. Default value is [UIColor
+ colorWithRed:0.0f green:0.0f blue:0.0f alpha:0.7f].
  */
-@property (copy, nonatomic) UIColor *maskLayerColor;
+@property(copy, nonatomic) UIColor *maskLayerColor;
 
 /**
- The line width used when stroking the path of the mask layer. Default value is 1.0.
+ The line width used when stroking the path of the mask layer. Default value is
+ 1.0.
  */
-@property (assign, nonatomic) CGFloat maskLayerLineWidth;
+@property(assign, nonatomic) CGFloat maskLayerLineWidth;
 
 /**
- The color to fill the stroked outline of the path of the mask layer, or nil for no stroking. Default valus is nil.
+ The color to fill the stroked outline of the path of the mask layer, or nil for
+ no stroking. Default valus is nil.
  */
-@property (copy, nonatomic, nullable) UIColor *maskLayerStrokeColor;
+@property(copy, nonatomic, nullable) UIColor *maskLayerStrokeColor;
 
 /**
  The rect of the mask.
- 
+
  @discussion Updating each time before the crop view lays out its subviews.
  */
-@property (assign, readonly, nonatomic) CGRect maskRect;
+@property(assign, readonly, nonatomic) CGRect maskRect;
 
 /**
  The path of the mask.
- 
+
  @discussion Updating each time before the crop view lays out its subviews.
  */
-@property (copy, readonly, nonatomic) UIBezierPath *maskPath;
+@property(copy, readonly, nonatomic) UIBezierPath *maskPath;
 
 /// -----------------------------------
 /// @name Accessing the Crop Attributes
@@ -122,45 +133,51 @@ typedef NS_ENUM(NSUInteger, RSKImageCropMode) {
 /**
  The mode for cropping. Default value is `RSKImageCropModeCircle`.
  */
-@property (assign, nonatomic) RSKImageCropMode cropMode;
+@property(assign, nonatomic) RSKImageCropMode cropMode;
 
 /**
  The crop rectangle.
- 
+
  @discussion The value is calculated at run time.
  */
-@property (readonly, nonatomic) CGRect cropRect;
+@property(readonly, nonatomic) CGRect cropRect;
 
 /**
  A value that specifies the current rotation angle of the image in radians.
- 
+
 @discussion The value is calculated at run time.
  */
-@property (readonly, nonatomic) CGFloat rotationAngle;
+@property(readonly, nonatomic) CGFloat rotationAngle;
 
 /**
- A floating-point value that specifies the current scale factor applied to the image.
- 
+ A floating-point value that specifies the current scale factor applied to the
+ image.
+
  @discussion The value is calculated at run time.
  */
-@property (readonly, nonatomic) CGFloat zoomScale;
+@property(readonly, nonatomic) CGFloat zoomScale;
 
 /**
- A Boolean value that determines whether the image will always fill the mask space. Default value is `NO`.
+ A Boolean value that determines whether the image will always fill the mask
+ space. Default value is `NO`.
  */
-@property (assign, nonatomic) BOOL avoidEmptySpaceAroundImage;
+@property(assign, nonatomic) BOOL avoidEmptySpaceAroundImage;
 
 /**
- A Boolean value that determines whether the mask applies to the image after cropping. Default value is `NO`.
+ A Boolean value that determines whether the mask applies to the image after
+ cropping. Default value is `NO`.
  */
-@property (assign, nonatomic) BOOL applyMaskToCroppedImage;
+@property(assign, nonatomic) BOOL applyMaskToCroppedImage;
 
 /**
- A Boolean value that controls whether the rotaion gesture is enabled. Default value is `NO`.
- 
- @discussion To support the rotation when `cropMode` is `RSKImageCropModeCustom` you must implement the data source method `imageCropViewControllerCustomMovementRect:`.
+ A Boolean value that controls whether the rotaion gesture is enabled. Default
+ value is `NO`.
+
+ @discussion To support the rotation when `cropMode` is `RSKImageCropModeCustom`
+ you must implement the data source method
+ `imageCropViewControllerCustomMovementRect:`.
  */
-@property (assign, getter=isRotationEnabled, nonatomic) BOOL rotationEnabled;
+@property(assign, getter=isRotationEnabled, nonatomic) BOOL rotationEnabled;
 
 /// -------------------------------
 /// @name Accessing the UI Elements
@@ -169,25 +186,26 @@ typedef NS_ENUM(NSUInteger, RSKImageCropMode) {
 /**
  The Title Label.
  */
-@property (strong, nonatomic, readonly) UILabel *moveAndScaleLabel;
+@property(strong, nonatomic) UILabel *moveAndScaleLabel;
 
 /**
  The Cancel Button.
  */
-@property (strong, nonatomic, readonly) UIButton *cancelButton;
+@property(strong, nonatomic, readonly) UIButton *cancelButton;
 
 /**
  The Choose Button.
  */
-@property (strong, nonatomic, readonly) UIButton *chooseButton;
+@property(strong, nonatomic, readonly) UIButton *chooseButton;
 
 /// -------------------------------------------
 /// @name Checking of the Interface Orientation
 /// -------------------------------------------
 
 /**
- Returns a Boolean value indicating whether the user interface is currently presented in a portrait orientation.
- 
+ Returns a Boolean value indicating whether the user interface is currently
+ presented in a portrait orientation.
+
  @return YES if the interface orientation is portrait, otherwise returns NO.
  */
 - (BOOL)isPortraitInterfaceOrientation;
@@ -195,49 +213,57 @@ typedef NS_ENUM(NSUInteger, RSKImageCropMode) {
 @end
 
 /**
- The `RSKImageCropViewControllerDataSource` protocol is adopted by an object that provides a custom rect and a custom path for the mask.
+ The `RSKImageCropViewControllerDataSource` protocol is adopted by an object
+ that provides a custom rect and a custom path for the mask.
  */
 @protocol RSKImageCropViewControllerDataSource <NSObject>
 
 /**
  Asks the data source a custom rect for the mask.
- 
+
  @param controller The crop view controller object to whom a rect is provided.
- 
+
  @return A custom rect for the mask.
- 
+
  @discussion Only valid if `cropMode` is `RSKImageCropModeCustom`.
  */
-- (CGRect)imageCropViewControllerCustomMaskRect:(RSKImageCropViewController *)controller;
+- (CGRect)imageCropViewControllerCustomMaskRect:
+    (RSKImageCropViewController *)controller;
 
 /**
  Asks the data source a custom path for the mask.
- 
+
  @param controller The crop view controller object to whom a path is provided.
- 
+
  @return A custom path for the mask.
- 
+
  @discussion Only valid if `cropMode` is `RSKImageCropModeCustom`.
  */
-- (UIBezierPath *)imageCropViewControllerCustomMaskPath:(RSKImageCropViewController *)controller;
+- (UIBezierPath *)imageCropViewControllerCustomMaskPath:
+    (RSKImageCropViewController *)controller;
 
 @optional
 
 /**
  Asks the data source a custom rect in which the image can be moved.
- 
+
  @param controller The crop view controller object to whom a rect is provided.
- 
+
  @return A custom rect in which the image can be moved.
- 
- @discussion Only valid if `cropMode` is `RSKImageCropModeCustom`. If you want to support the rotation  when `cropMode` is `RSKImageCropModeCustom` you must implement it. Will be marked as `required` in version `2.0.0`.
+
+ @discussion Only valid if `cropMode` is `RSKImageCropModeCustom`. If you want
+ to support the rotation  when `cropMode` is `RSKImageCropModeCustom` you must
+ implement it. Will be marked as `required` in version `2.0.0`.
  */
-- (CGRect)imageCropViewControllerCustomMovementRect:(RSKImageCropViewController *)controller;
+- (CGRect)imageCropViewControllerCustomMovementRect:
+    (RSKImageCropViewController *)controller;
 
 @end
 
 /**
- The `RSKImageCropViewControllerDelegate` protocol defines messages sent to a image crop view controller delegate when crop image was canceled or the original image was cropped.
+ The `RSKImageCropViewControllerDelegate` protocol defines messages sent to a
+ image crop view controller delegate when crop image was canceled or the
+ original image was cropped.
  */
 @protocol RSKImageCropViewControllerDelegate <NSObject>
 
@@ -246,22 +272,31 @@ typedef NS_ENUM(NSUInteger, RSKImageCropMode) {
 /**
  Tells the delegate that crop image has been canceled.
  */
-- (void)imageCropViewControllerDidCancelCrop:(RSKImageCropViewController *)controller;
+- (void)imageCropViewControllerDidCancelCrop:
+    (RSKImageCropViewController *)controller;
 
 /**
  Tells the delegate that the original image will be cropped.
  */
-- (void)imageCropViewController:(RSKImageCropViewController *)controller willCropImage:(UIImage *)originalImage;
+- (void)imageCropViewController:(RSKImageCropViewController *)controller
+                  willCropImage:(UIImage *)originalImage;
 
 /**
- Tells the delegate that the original image has been cropped. Additionally provides a crop rect used to produce image.
+ Tells the delegate that the original image has been cropped. Additionally
+ provides a crop rect used to produce image.
  */
-- (void)imageCropViewController:(RSKImageCropViewController *)controller didCropImage:(UIImage *)croppedImage usingCropRect:(CGRect)cropRect;
+- (void)imageCropViewController:(RSKImageCropViewController *)controller
+                   didCropImage:(UIImage *)croppedImage
+                  usingCropRect:(CGRect)cropRect;
 
 /**
- Tells the delegate that the original image has been cropped. Additionally provides a crop rect and a rotation angle used to produce image.
+ Tells the delegate that the original image has been cropped. Additionally
+ provides a crop rect and a rotation angle used to produce image.
  */
-- (void)imageCropViewController:(RSKImageCropViewController *)controller didCropImage:(UIImage *)croppedImage usingCropRect:(CGRect)cropRect rotationAngle:(CGFloat)rotationAngle;
+- (void)imageCropViewController:(RSKImageCropViewController *)controller
+                   didCropImage:(UIImage *)croppedImage
+                  usingCropRect:(CGRect)cropRect
+                  rotationAngle:(CGFloat)rotationAngle;
 
 @end
 
